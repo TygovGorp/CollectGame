3b5dd2e2e5f58734b1461c430ddfe85f3a5051e2
gamer013NL <Tygo.vanGorp@outlook.com>
2023-10-08 16:50:56 +0200

    added template

---
+++ CollectGame/game.cpp
@@ -0,0 +1,30 @@
+#include "game.h"
+#include "surface.h"
+#include <cstdio> //printf
+
+namespace Tmpl8
+{
+	// -----------------------------------------------------------
+	// Initialize the application
+	// -----------------------------------------------------------
+	void Game::Init()
+	{
+	}
+	
+	// -----------------------------------------------------------
+	// Close down application
+	// -----------------------------------------------------------
+	void Game::Shutdown()
+	{
+	}
+
+	// -----------------------------------------------------------
+	// Main application tick function
+	// -----------------------------------------------------------
+	void Game::Tick(float deltaTime)
+	{
+		// clear the graphics window
+		screen->Clear(0);
+
+	}
+};
