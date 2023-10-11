304f2a8a3835d65e3654ad092bcd82f81faecb2d
Tygo van Gorp <64016141@cambreurmail.nl>
2023-10-11 13:29:45 +0200

    object class is done

--- CollectGame/object.cpp
+++ CollectGame/object.cpp
@@ -2,5 +2,48 @@
 
 namespace Tmpl8
 {
+	object::object()
+	{
+		loc = { 1, 1 };
+		collisionBox[0] = { 1, 1 };
+		collisionBox[1] = { 1, 1 };
+	}
+	object::object(vec2 LOC, vec2 pointA, vec2 pointB)
+	{
+		loc = LOC;
+		collisionBox[0] = pointA;
+		collisionBox[1] = pointB;
+	}
+	object::object(int LOCx, int LOCy, int Ax, int Ay, int Bx, int By)
+	{
+		loc.x = LOCx;
+		loc.y = LOCy;
+		collisionBox[0].x = Ax;
+		collisionBox[0].y = Ay;
+		collisionBox[1].x = Bx;
+		collisionBox[1].y = By;
+	}
 
+	void object::setLoc(vec2 LOC)
+	{
+		loc = LOC;
+	}
+	void object::setLoc(int LOCx, int LOCy)
+	{
+		loc.x = LOCx;
+		loc.y = LOCy;
+	}
+
+	void object::setCollisionBox(vec2 A, vec2 B)
+	{
+		collisionBox[0] = A;
+		collisionBox[1] = B;
+	}
+	void object::setCollisionBox(int Ax, int Ay, int Bx, int By)
+	{
+		collisionBox[0].x = Ax;
+		collisionBox[0].y = Ay;
+		collisionBox[1].x = Bx;
+		collisionBox[1].y = By;
+	}
 }
--- CollectGame/object.h
+++ CollectGame/object.h
@@ -6,8 +6,21 @@ namespace Tmpl8
 	class object
 	{
 	public:
+		object();
+		object(vec2 LOC, vec2 pointA, vec2 pointB);
+		object(int LOCx, int LOCy, int Ax, int Ay, int Bx, int By);
+	
+		void setLoc(vec2 LOC);
+		void setLoc(int LOCx, int LOCy);
+
+		void setCollisionBox(vec2 A, vec2 B);
+		void setCollisionBox(int Ax, int Ay, int Bx, int By);
+
+		vec2 getLoc() { vec2 loc; };
+		vec2 getCollisionBox(int pointNumber) { return collisionBox[pointNumber]; };
 
 	protected:
-		vec2 //was working here
+		vec2 loc;
+		vec2 collisionBox[2];
 	};
 }
